c5f9b6ce15d9259f24638d1e6d318601848f287c
Caitlin <10053862+con-cat@users.noreply.github.com>
2023-02-01 22:14:27 +1100

    The basics of fetching Transactions

--- exporter/__main__.py
+++ exporter/__main__.py
@@ -1,3 +1,15 @@
 import app
+import datetime
 
-app.test()
+
+accounts = app.client.accounts()
+account = accounts[0]
+
+transactions = app.get_transactions_for_account(
+    account,
+    start_date=datetime.date(2023, 1, 1),
+    end_date=datetime.date(2023, 1, 31),
+)
+
+for transaction in transactions:
+    print(transaction)
--- exporter/app.py
+++ exporter/app.py
@@ -1,6 +1,9 @@
-import upbankapi
+from upbankapi import Client, models
+import datetime
 
-client = upbankapi.Client()
+TIMEZONE = datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
+
+client = Client()
 
 
 def test():
@@ -9,3 +12,19 @@ def test():
         print(f"Authorized: {user_id}")
     except upbankapi.NotAuthorizedException:
         print("The token is invalid")
+
+
+def get_transactions_for_account(
+    account: models.Account, start_date: datetime.date, end_date: datetime.date
+) -> models.PaginatedList[models.Transaction]:
+    since = datetime.datetime.combine(
+        start_date,
+        datetime.datetime.min.time(),
+        tzinfo=TIMEZONE,
+    )
+    until = datetime.datetime.combine(
+        end_date + datetime.timedelta(days=1),
+        datetime.datetime.min.time(),
+        tzinfo=TIMEZONE,
+    )
+    return account.transactions(since=since, until=until)

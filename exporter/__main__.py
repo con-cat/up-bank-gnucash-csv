7aecbdb7e4db247b0b2200a445683bde12eb7e3d
Caitlin <10053862+con-cat@users.noreply.github.com>
2023-02-02 00:32:30 +1100

    Make it a CLI with date arguments; write files for all accounts

--- exporter/__main__.py
+++ exporter/__main__.py
@@ -1,19 +1,49 @@
 import app
+import argparse
 import datetime
 
 
-start_date = datetime.date(2023, 1, 1)
-end_date = datetime.date(2023, 1, 31)
-accounts = app.client.accounts()
-account = accounts[0]
-
-transactions = app.get_transactions_for_account(
-    account,
-    start_date=datetime.date(2023, 1, 1),
-    end_date=datetime.date(2023, 1, 31),
+parser = argparse.ArgumentParser(
+    prog="up-bank-gnucash-csv",
+    description="Export transactions from Up Bank as CSVs formatted for GnuCash",
+)
+parser.add_argument(
+    "--start",
+    type=datetime.date.fromisoformat,
+    help="Start date to query, YYYY-MM-DD format",
+    required=True,
+)
+parser.add_argument(
+    "--end",
+    type=datetime.date.fromisoformat,
+    help="End date to query, YYYY-MM-DD format",
+    required=True,
 )
 
-filename = f"{account.name}_{start_date.isoformat()}_{end_date.isoformat()}.csv"
 
-app.write_csv(filename=filename, transactions=transactions)
-print(f"CSV written: {filename}")
+def write_csvs() -> None:
+    args = parser.parse_args()
+    try:
+        app.client.ping()
+    except upbankapi.UpBankException as e:
+        print(f"Error connecting to the API: {e}")
+        return
+
+    accounts = app.client.accounts()
+    print(f"💁‍♀️ Writing CSVs for {len(accounts)} accounts.")
+
+    for account in accounts:
+        transactions = app.get_transactions_for_account(
+            account,
+            start_date=args.start,
+            end_date=args.end,
+        )
+        # TODO: account name in filename: strip out emojis and replace spaces
+        filename = f"{account.name}_{args.start.isoformat()}_{args.end.isoformat()}.csv"
+        app.write_csv(filename=filename, transactions=transactions)
+        print(f"📄 CSV written: {filename}")
+
+    print("All done 🎉")
+
+
+write_csvs()
